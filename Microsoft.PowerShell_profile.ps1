d3c1e8ec517457cb775dce14341011f8fb2842e9
Nyle Wagjiani <NyleWagjiani@gmail.com>
2024-10-13 16:56:46 +0100

    Rename repo

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,7 +7,7 @@ if (-not $global:canConnectToGitHub) {
 }
 
 try {
-  $url = "https://raw.githubusercontent.com/PantiesIsStoopid/PowershellProfile/refs/heads/master/Microsoft.PowerShell_profile.ps1"
+  $url = "https://raw.githubusercontent.com/PantiesIsStoopid/Powershell/refs/heads/master/Microsoft.PowerShell_profile.ps1"
   $oldhash = Get-FileHash $PROFILE
   Invoke-RestMethod $url -OutFile "$env:temp/Microsoft.PowerShell_profile.ps1"
   $newhash = Get-FileHash "$env:temp/Microsoft.PowerShell_profile.ps1"
