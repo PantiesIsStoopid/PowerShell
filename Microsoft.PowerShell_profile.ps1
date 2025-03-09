2cfba90bdca73f8b57ec586defc9d2c607695d3f
Nyle <nylewagjiani@gmail.com>
2025-03-09 18:34:58 +0000

    more clarity

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -82,7 +82,7 @@ Set-PSFzfOption -PSReadlineChordProvider "Ctrl+f" -PSReadlineChordReverseHistory
 
 Set-PSReadLineKeyHandler -Chord Ctrl+g -ScriptBlock { Grep }
 
-$ENV:FZF_DEFAULT_OPTS=@"
+$ENV:FZF_DEFAULT_OPTS = @"
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
 --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
@@ -445,7 +445,7 @@ Additional Functions:
 Keybinds:
 - Ctrl + F: Opens fuzzy finder.
 - Ctrl + R: Fuzzy find through past command history.
-- Ctrl + G: Lets you grep in your current folder.
+- Ctrl + G: Lets you FZF with preview in your current folder.
 
 CheatSheet: Displays a list of all the most common commands.
 
